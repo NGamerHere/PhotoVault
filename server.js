88df2f53572329c63e3d49440a0348321fd113b4
NGamerHere <codingdatta@gmail.com>
2024-09-07 22:37:13 +0530

    inital commit

---
+++ server.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import path from 'path';
+import bodyParser from "body-parser";
+import { fileURLToPath } from 'url';
+import multer from 'multer';
+import cors from 'cors';
+
+const app=express();
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage: storage });
+
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello from the server!' });
+});
+
+
+
+app.post('/images/upload', upload.single('file'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).send('No file uploaded.');
+    }
+    res.send(`File uploaded successfully: ${req.file.filename}`);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+
+app.listen(3000, () => {
+    console.log(`Server is running on port 3000`);
+});
---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import {useState} from "react";
+import axios from "axios";
+
+function App() {
+    const [file,setFile]=useState(null);
+    const inputHandle= (event)=> {
+        setFile(event.target.files[0]);
+    }
+    const handleSubmit=()=>{
+        const formData=new FormData();
+        formData.append('file',file);
+        axios.post('/images/upload',formData,{headers: {'Content-Type': 'multipart/form-data',}})
+            .then((res)=>{
+            console.log(res.data);
+        }).catch((e)=>{
+            console.error(e.response.data);
+        })
+    }
+  return (
+    <>
+        <input type='file' onChange={inputHandle}  />
+        <button onClick={handleSubmit} >submit</button>
+    </>
+  )
+}
+
+export default App
