7f7b4c8b05936c98f248699f28b84bad17b2b607
NGamerHere <codingdatta@gmail.com>
2024-11-08 17:45:58 +0530

    comment out the dashboard code as it was leaking the complete token details

--- api/route/Dashboard.js
+++ api/route/Dashboard.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import jwtDecoding from "../middleware/JwtDecoding.js";
+//import jwtDecoding from "../middleware/JwtDecoding.js";
 
 const Dashboard = express.Router();
 
 
-Dashboard.get('/api/dashboard', jwtDecoding, (req, res) => {
-    res.status(200).json({ message: 'Access granted to dashboard',data:req['tokenData'] });
-});
+// Dashboard.get('/api/dashboard', jwtDecoding, (req, res) => {
+//     res.status(200).json({ message: 'Access granted to dashboard',data:req['tokenData'] });
+// });
 
 export default Dashboard;
